34e2728666090964af1428bda37de4b17c5f4511
MaxLee0325 <icmaxlee@gmail.com>
2024-06-17 14:37:04 -0300

    helper script added

---
+++ BashScripts/file_filter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then
+	echo "Invalid number of arguments"
+	exit 1
+fi
+
+source_dir=$1
+file_type=$2
+
+output_folder="$source_dir/output" 
+
+mkdir -p "$output_folder"
+
+cp -r "$source_dir"/*."$file_type" "$output_folder"
+
+echo "Copy completed."
+
+
